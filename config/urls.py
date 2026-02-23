78d7ef02e8c9ee9c1a84df580eb62198a2014995
Tahir Ali <tahirbacha506@gmail.com>
2026-02-23 14:35:48 +0500

    Add token authentication endpoints for JWT

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework_simplejwt.views import (TokenRefreshView)
+from payments.views import CustomTokenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/token/', CustomTokenView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/payments/', include('payments.urls'))
 ]
