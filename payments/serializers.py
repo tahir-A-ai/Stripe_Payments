2dcd1cd3dfb494be2a855623971f8c4338094b23
Tahir Ali <tahirbacha506@gmail.com>
2026-02-23 14:36:08 +0500

    Add CustomTokenSerializer to include username in JWT token

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class CheckoutSessionSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
     quantity = serializers.IntegerField(min_value=1, default=1)
+
+class CustomTokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        return token
