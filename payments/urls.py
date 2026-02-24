a11eea2f473de7cd78c7a497b7cfb9c2b8b77883
Tahir Ali <tahirbacha506@gmail.com>
2026-02-24 10:27:54 +0500

    Remove commented StripeCallbackView from URL patterns

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import( 
-    CreateCheckoutSessionView, ConnectStripeView, 
-    StripeCallbackView, CreateVendorPayoutView,
+    CreateCheckoutSessionView, ConnectStripeView,
+    CreateVendorPayoutView,
     CreatePlatformPayoutView,
     )
 from django.views.generic import TemplateView
@@ -11,7 +11,7 @@ urlpatterns = [
     path('success/', TemplateView.as_view(template_name='payments/success.html'), name='payment-success'),
     path('cancel/', TemplateView.as_view(template_name='payments/cancel.html'), name='payment-cancel'),
     path('connect-stripe/', ConnectStripeView.as_view(), name='connect-stripe'),
-    path('oauth/callback/', StripeCallbackView.as_view(), name='stripe-callback'),
+    # path('oauth/callback/', StripeCallbackView.as_view(), name='stripe-callback'),
     path('create-vendor-payout/', CreateVendorPayoutView.as_view(), name='create-vendor-payout'),
     path('create-platform-payout/', CreatePlatformPayoutView.as_view(), name='create-platform-payout'),
 ]
