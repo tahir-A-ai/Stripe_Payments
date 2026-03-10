ad7c1956d51ff8657fa391f7db57bbe9d07a2794
Tahir Ali <tahirbacha506@gmail.com>
2026-03-10 08:42:48 +0500

    Refactor email configuration to use environment variables for sensitive data

--- config/settings.py
+++ config/settings.py
@@ -48,9 +48,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hab808443@gmail.com'
-EMAIL_HOST_PASSWORD = '1234567a-'
-DEFAULT_FROM_EMAIL = 'hab808443@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 
 REST_AUTH = {
